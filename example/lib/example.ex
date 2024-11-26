3bbeb9acc8cddd7466ddcf09356f92ea68fecc0c
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 14:41:29 -0500

    hex usage

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,9 +1,11 @@
 defmodule Example do
   use Application
+  alias UUID
 
   def start(_type, _args) do
     # code
     IO.puts(Example.hello)
+    IO.puts(UUID.uuid4())
     # {:ok, self()}
     Supervisor.start_link([], strategy: :one_for_one)
   end
