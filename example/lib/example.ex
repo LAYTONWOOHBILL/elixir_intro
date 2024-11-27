36f42766663f1f87f54cca6e18c6eb0df216e662
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 23:36:17 -0500

    List

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -7,19 +7,19 @@ defmodule Example do
   end
 
   def main do
-    correct_answer = :rand.uniform(10)
-    # IO.puts("The correct answer is #{correct_answer}")
-    guess = IO.gets("Guess the number between 1 and 10: ") |> String.trim() |> Integer.parse()
-    # IO.inspect(guess)
-    case guess do
-      {result, ""} -> IO.puts("You guessed #{result}")
-        if result == correct_answer do
-          IO.puts("You guessed correctly!")
-        else
-          IO.puts("You lost! The correct answer is #{correct_answer}")
-        end
-      {result, other}  -> IO.puts("parse partially successful #{result} and #{other}")
-      :error -> IO.puts("parse error")
+    grade = [1, 2, 3, 4, 5]
+    new = for n <- grade do
+      n+5
     end
+    IO.inspect(new)
+    new = new ++ [125]
+    new = new ++ [120, 121, 122]
+    IO.inspect(new)
+    final = new -- [121]
+    final = [5| new]
+    new = new ++ [1]
+    IO.inspect(final)
+    even = for n <- final, rem(n, 2) == 0, do: n
+    IO.inspect(even)
   end
 end
