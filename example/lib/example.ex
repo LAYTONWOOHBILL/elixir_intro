be16b938cbaf8995a2301a6b6aa9a38673f8b755
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 23:45:33 -0500

    Function 2

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -6,10 +6,26 @@ defmodule Example do
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
+  def sum_and_average(numbers) do
+    sum = Enum.sum(numbers)
+    average = sum / Enum.count(numbers)
+    {sum, average}
+  end
+
+  def print_number(numbers) do
+    numbers |> Enum.join(" ") |> IO.puts()
+  end
+
+  def get_numbers_from_user do
+    IO.puts("Enter numbers separated by spaces:")
+    input = IO.gets("") |> String.trim()
+    String.split(input, " ") |> Enum.map(&String.to_integer/1)
+  end
+
   def main do
-    number = ["1", "2", "3", "4", "5"]
-    IO.inspect(number)
-    res = Enum.map(number, &String.to_integer/1)
-    IO.inspect(res)
+    numbers = get_numbers_from_user()
+    IO.inspect(numbers)
+    print_number(numbers)
+    IO.inspect(sum_and_average(numbers))
   end
 end
