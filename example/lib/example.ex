af505970acabb4fec032ff2e5dd93ed0b4e63356
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 23:24:24 -0500

    case

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,45 +1,25 @@
-defmodule Membership do
-  defstruct [:type, :price]
-end
-
-defmodule User do
-  defstruct [:name, :membership]
-end
-
 defmodule Example do
   use Application
-  alias UUID
 
   def start(_type, _args) do
-    # code
     Example.main()
-    # {:ok, self()}
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def main do
-
-    memberships = %{
-      bronze: :bronze,
-      silver: :silver,
-      gold: :gold,
-      platinum: :platinum
-    }
-
-    gold_membership = %Membership{type: :gold, price: 30}
-    silver_membership = %Membership{type: :silver, price: 20}
-    bronze_membership = %Membership{type: :bronze, price: 10}
-    platinum_membership = %Membership{type: :platinum, price: 40}
-
-    users = [
-      %User{name: "John", membership: gold_membership},
-      %User{name: "Jane", membership: silver_membership},
-      %User{name: "Jim", membership: bronze_membership},
-      %User{name: "Jill", membership: platinum_membership}
-    ]
-
-    Enum.each(users, fn %User{name: name, membership: membership} ->
-      IO.puts("#{name} is a #{membership.type} member. price is #{membership.price}")
-    end)
+    correct_answer = :rand.uniform(10)
+    # IO.puts("The correct answer is #{correct_answer}")
+    guess = IO.gets("Guess the number between 1 and 10: ") |> String.trim() |> Integer.parse()
+    # IO.inspect(guess)
+    case guess do
+      {result, ""} -> IO.puts("You guessed #{result}")
+        if result == correct_answer do
+          IO.puts("You guessed correctly!")
+        else
+          IO.puts("You lost! The correct answer is #{correct_answer}")
+        end
+      {result, other}  -> IO.puts("parse partially successful #{result} and #{other}")
+      :error -> IO.puts("parse error")
+    end
   end
 end
