00a7f9d02961bdbcc54e6b3517fe0f3a0f5e925e
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 23:39:34 -0500

    functional

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -7,19 +7,9 @@ defmodule Example do
   end
 
   def main do
-    grade = [1, 2, 3, 4, 5]
-    new = for n <- grade do
-      n+5
-    end
-    IO.inspect(new)
-    new = new ++ [125]
-    new = new ++ [120, 121, 122]
-    IO.inspect(new)
-    final = new -- [121]
-    final = [5| new]
-    new = new ++ [1]
-    IO.inspect(final)
-    even = for n <- final, rem(n, 2) == 0, do: n
-    IO.inspect(even)
+    number = ["1", "2", "3", "4", "5"]
+    IO.inspect(number)
+    res = Enum.map(number, &String.to_integer/1)
+    IO.inspect(res)
   end
 end
