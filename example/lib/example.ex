707497718267718671c2ea87e6f7afbd90ef18f0
laytonwoohbill <wwu833412@gmail.com>
2024-11-26 23:01:40 -0500

    struct

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -1,3 +1,11 @@
+defmodule Membership do
+  defstruct [:type, :price]
+end
+
+defmodule User do
+  defstruct [:name, :membership]
+end
+
 defmodule Example do
   use Application
   alias UUID
@@ -10,21 +18,28 @@ defmodule Example do
   end
 
   def main do
-    memberships = {:bronze, :silver}
-    memberships = Tuple.append(memberships, :gold)
-    IO.inspect(memberships)
 
-    price = {5, 10, 15}
-    avg = Tuple.sum(price) / tuple_size(price)
-    IO.inspect(avg)
+    memberships = %{
+      bronze: :bronze,
+      silver: :silver,
+      gold: :gold,
+      platinum: :platinum
+    }
 
-    IO.puts("#{elem(memberships, 0)} #{elem(memberships, 1)} #{elem(memberships, 2)} is #{avg}")
+    gold_membership = %Membership{type: :gold, price: 30}
+    silver_membership = %Membership{type: :silver, price: 20}
+    bronze_membership = %Membership{type: :bronze, price: 10}
+    platinum_membership = %Membership{type: :platinum, price: 40}
 
-    users = [{"John", :gold}, {"Jane", :silver}, {"Jim", :bronze}]
-    users = List.insert_at(users, 0, {"Jill", :platinum})
+    users = [
+      %User{name: "John", membership: gold_membership},
+      %User{name: "Jane", membership: silver_membership},
+      %User{name: "Jim", membership: bronze_membership},
+      %User{name: "Jill", membership: platinum_membership}
+    ]
 
-    Enum.each(users, fn {name, membership} ->
-      IO.puts("#{name} is a #{membership} member")
+    Enum.each(users, fn %User{name: name, membership: membership} ->
+      IO.puts("#{name} is a #{membership.type} member. price is #{membership.price}")
     end)
   end
 end
